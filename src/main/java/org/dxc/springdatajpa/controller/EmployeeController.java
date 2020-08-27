19390d02b5c3fc39e1f23e314a60b727a01cc78f
sherine <sherinein@gmail.com>
2020-08-27 10:04:51 +0530

    firt commit changes in controller

--- src/main/java/org/dxc/springdatajpa/controller/EmployeeController.java
+++ src/main/java/org/dxc/springdatajpa/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package org.dxc.springdatajpa.controller;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 import org.dxc.springdatajpa.entity.Employee;
 import org.dxc.springdatajpa.repository.EmployeeRepository;
@@ -19,29 +20,34 @@ public class EmployeeController {
 	@Autowired
 	EmployeeRepository employeeRepository;
 	
-	
+	private Logger logger = Logger.getLogger(getClass().getName());
 	@RequestMapping("/findall")
 	public List<Employee> findAllEmployee(){
+		logger.info("==========findAllEmployee===========");
 		return (List<Employee>)employeeRepository.findAll();
 	}
 	
 	@RequestMapping(value="/save",method=RequestMethod.POST)
 	public HttpStatus insertEmployee(@RequestBody Employee employee) {
+		logger.info("==========insertEmployee===========" );
 		boolean status=employeeRepository.save(employee)!=null;
 		return  status ? HttpStatus.CREATED : HttpStatus.BAD_REQUEST;
 	}
 	@RequestMapping("/findbyid")
 	public Optional<Employee> findById(@RequestParam("id")long id){
+		logger.info("==========findById===========" );
 		Optional<Employee> result=employeeRepository.findById(id);
 		return result;
 	}
 	
 	@RequestMapping("/findbylastname")
 	public List<Employee> fetchDataByName(@RequestParam("lastname")String lastName){
+		logger.info("==========fetchDataByName===========" );
 		return employeeRepository.findByLastName(lastName);
 	}
 	@RequestMapping("/findbyage")
 	public List<Employee> fetchDataByAge(@RequestParam("age")int age){
+		logger.info("==========fetchDataByAge===========");
 		return employeeRepository.findByAge(age);
 	}
 	
